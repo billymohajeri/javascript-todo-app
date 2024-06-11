84f90e19a8280cf8ed8b4d1c5d91d8aedb0414d5
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-11 18:25:44 +0300

    Display existing todos

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const todos=['Todo 1','Todo 2','Todo 3']
+const todos = ["Todo 1", "Todo 2", "Todo 3"];
 
-const listContainer=document.querySelector(".list-container")
-
+const listContainer = document.querySelector(".list-container");
+todos.forEach((todo) => {
+  const todoLabel = document.createElement("label");
+  todoLabel.textContent = todo;
+  console.log(todoLabel);
+  listContainer.appendChild(todoLabel);
+});
 
 console.log(todos);
 console.log(listContainer);
