9508ab7eac5c34a4c8418ff460c809460d9ca54d
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-11 19:23:18 +0300

    Add delete button

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const todos = ["Todo 1", "Todo 2", "Todo 3"];
 
 const listContainer = document.querySelector(".list-container");
-todos.forEach((todo) => {
-  let todoItem = document.createElement("li");
-  todoItem.innerHTML = `<input type="checkbox" id="${todo}"><label for="${todo}">${todo}</label>`;
-  listContainer.appendChild(todoItem);
-});
 
-console.log(todos);
-console.log(listContainer);
+const updateList = () => {
+  todos.forEach((todo) => {
+    let todoItem = document.createElement("li");
+    todoItem.innerHTML = `<input type="checkbox" id="${todo}"><label for="${todo}">${todo}</label><input type="button" value="Delete">`;
+    listContainer.appendChild(todoItem);
+  });
+};
+window.addEventListener("load", updateList);
+
+
+
+// console.log(todos);
+// console.log(listContainer);
