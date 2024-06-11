86919742831cfd339663d70be5265cbc1f601cd2
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-11 22:19:13 +0300

    Implement edit todo

--- index.js
+++ index.js
@@ -20,11 +20,35 @@ const handleDelete = (deletedItem) => {
   }
   updateList();
 };
+
+const handleEdit = (todoToEdit, editBtn) => {
+  const label = editBtn.previousElementSibling.previousElementSibling;
+  if (editBtn.value === "Edit") {
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = todoToEdit;
+    label.replaceWith(input);
+    editBtn.value = "Save";
+  } else {
+    const newValue =
+      editBtn.previousElementSibling.previousElementSibling.value.trim();
+    if (newValue) {
+      const index = todos.indexOf(todoToEdit);
+      if (index !== -1) {
+        todos[index] = newValue;
+      }
+    }
+    updateList();
+  }
+};
+
 const updateList = () => {
   listContainer.innerHTML = "";
   todos.forEach((todo) => {
     let todoItem = document.createElement("li");
-    todoItem.innerHTML = `<input type="checkbox" id="${todo}"><label for="${todo}">${todo}</label><input type="button" value="Delete" class="del-btn" data-todo="${todo}">`;
+    todoItem.innerHTML = `<input type="checkbox" id="${todo}">
+    <label for="${todo}">${todo}</label><input type="button" value="Delete" class="del-btn" data-todo="${todo}">
+    <input type="button" value="Edit" class="edit-btn" data-todo="${todo}">`;
     listContainer.appendChild(todoItem);
   });
   const delBtns = document.querySelectorAll(".del-btn");
@@ -34,6 +58,15 @@ const updateList = () => {
       handleDelete(todo);
     });
   });
+
+  const editBtns = document.querySelectorAll(".edit-btn");
+  editBtns.forEach((editBtn) => {
+    editBtn.addEventListener("click", (e) => {
+      const todo = e.target.getAttribute("data-todo");
+      handleEdit(todo, e.target);
+    });
+  });
+
   counter.innerHTML = todos.length;
 };
 
