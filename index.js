e4d5702fa2d3340c392f13fd426caa78add70136
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-06-13 00:09:24 +0300

    Update edit function

--- index.js
+++ index.js
@@ -20,27 +20,31 @@ addBtn.addEventListener("click", () => {
 
 const handleDelete = (deletedItemID) => {
   todos = todos.filter((todo) => {
-    todo.id !== deletedItemID;
+    return todo.id !== deletedItemID;
   });
 
   updateList();
 };
 
-const handleEdit = (todoToEdit, editBtn) => {
-  const label = editBtn.previousElementSibling.previousElementSibling;
+const handleEdit = (todoToEditID) => {
+  const label = document.getElementById(`lbl${todoToEditID}`);
+  const editBtn = document.getElementById(`edt${todoToEditID}`);
   if (editBtn.value === "Edit") {
     const input = document.createElement("input");
     input.type = "text";
-    input.value = todoToEdit;
+    input.id = `input${todoToEditID}`;
+    input.value = label.textContent;
     label.replaceWith(input);
     editBtn.value = "Save";
   } else {
-    const newValue =
-      editBtn.previousElementSibling.previousElementSibling.value.trim();
+    const input = document.getElementById(`input${todoToEditID}`);
+    const newValue = input.value.trim();
     if (newValue) {
-      const index = todos.indexOf(todoToEdit);
+      const index = todos.findIndex(
+        (todo) => todo.id === parseInt(todoToEditID)
+      );
       if (index !== -1) {
-        todos[index] = newValue;
+        todos[index].title = newValue;
       }
     }
     updateList();
@@ -52,23 +56,24 @@ const updateList = () => {
   todos.forEach((todo) => {
     const { id, title, done } = todo;
     let todoItem = document.createElement("li");
-    todoItem.innerHTML = `<input type="checkbox" id="${id}" checked="${done}">
-    <label for="${id}">${title}</label><input type="button" value="Delete" class="del-btn" id="${id}">
-    <input type="button" value="Edit" class="edit-btn" data-todo="${todo}">`;
+    todoItem.innerHTML = `<input type="checkbox" checked="${done}">
+    <label for="${id}" id="lbl${id}">${title}</label><input type="button" value="Delete" class="del-btn" id="del${id}">
+    <input type="button" value="Edit" class="edit-btn" id="edt${id}">`;
     listContainer.appendChild(todoItem);
   });
   const delBtns = document.querySelectorAll(".del-btn");
   delBtns.forEach((delBtn) => {
     delBtn.addEventListener("click", () => {
-      handleDelete(delBtn.id);
+      const delID = parseInt(delBtn.id.replace("del", ""));
+      handleDelete(delID);
     });
   });
 
   const editBtns = document.querySelectorAll(".edit-btn");
   editBtns.forEach((editBtn) => {
-    editBtn.addEventListener("click", (e) => {
-      const todo = e.target.getAttribute("data-todo");
-      handleEdit(todo, e.target);
+    editBtn.addEventListener("click", () => {
+      const edtID = parseInt(editBtn.id.replace("edt", ""));
+      handleEdit(edtID);
     });
   });
 
